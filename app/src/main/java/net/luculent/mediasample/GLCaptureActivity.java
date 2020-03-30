9c57eb9533921a907837acadfa9ec5638bd063e2
xiayanlei <399904882@qq.com>
2020-03-30 16:19:14 +0800

    demo例子修改

--- app/src/main/java/net/luculent/mediasample/GLCaptureActivity.java
+++ app/src/main/java/net/luculent/mediasample/GLCaptureActivity.java
@@ -1,9 +1,13 @@
 package net.luculent.mediasample;
 
+import android.graphics.BitmapFactory;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.xyl.camera.video.view.opengl.drawer.BitmapDrawer;
+import com.xyl.camera.video.view.opengl.render.BaseRender;
 
 import net.luculent.mediasample.tools.ActivityUtils;
 
@@ -25,5 +29,9 @@ public class GLCaptureActivity extends AppCompatActivity {
 
     private void initView() {
         surfaceView = findViewById(R.id.gl_surface);
+        surfaceView.setEGLContextClientVersion(2);
+        surfaceView.setRenderer(new BaseRender<>(new BitmapDrawer(BitmapFactory.decodeResource
+                (getResources(), R.mipmap.img_00))));
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
     }
 }
